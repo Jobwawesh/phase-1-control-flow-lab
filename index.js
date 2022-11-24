84ab705332f50d41888adf35b0aaea2349e231c5
Jobwawesh <jobnjunge15@gmail.com>
2022-11-24 14:41:46 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+
+    let result;
+    if (distance <= 400){
+      result = 'This one is on me!';
+      return result
+  }else if (distance > 2000 && distance < 2500){
+    result = 'I will gladly take your thirty bucks.';
+    return result
+  }
+  else {
+  result = 'No can do.'
+  return result
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if(city === "NYC"){
+    return "Ok, sounds good.";
+  }
+  else{
+    return "No go.";
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':{
+      return "Thank you so much.";
+    }break;
+    case 'not as generous':{
+      return "Thank you.";
+    }
+    default :{
+      return "Bye.";
+    }
+  }
 }
